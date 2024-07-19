f08a050184f1535924e86cdc17f5e2fdae4caa1b
zainnadeem786 <zainnadeemzainnadeem80@gmail.com>
2024-07-19 15:02:14 +0500

    Taisu Electronics

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import About, Product, Contact, Brand, Category, ProductDetails
 
-# Register your models here.
+
+
+admin.site.register(About)
+admin.site.register(Product)
+admin.site.register(Contact)
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(ProductDetails)
